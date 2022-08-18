72123eed65450df397ec0512c60e69b14cc416e8
Alpcan BELEN <alpcan237@gmail.com>
2022-08-18 14:52:25 +0300

    home page

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,6 +1,27 @@
-﻿namespace net_e_commerce.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_e_commerce.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser: IdentityUser
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+        [NotMapped]
+        public string Fullname
+        {
+            get
+            {
+                return Name + " " + Surname;
+            }
+        }
     }
 }
