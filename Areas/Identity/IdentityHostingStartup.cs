6f92c1ea4c7a7fda2f6ca7be14474926d4c76dcd
Alpcan BELEN <alpcan237@gmail.com>
2022-08-22 18:19:49 +0300

    register/login

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using net_e_commerce.Data;
+
+[assembly: HostingStartup(typeof(net_e_commerce.Areas.Identity.IdentityHostingStartup))]
+namespace net_e_commerce.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
